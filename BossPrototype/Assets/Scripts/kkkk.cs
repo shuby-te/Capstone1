60d769e61d638c6496e5715de5fb8cd9ac9a8e86
taesoo-jeong <jp430612@naver.com>
2023-09-21 19:43:58 +0900

    lastlastlast
    lastlasrt

--- BossPrototype/Assets/Scripts/kkkk.cs
+++ BossPrototype/Assets/Scripts/kkkk.cs
@@ -7,7 +7,7 @@ public class kkkk : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //erwqqwerqwererqw
     }
 
     // Update is called once per frame
