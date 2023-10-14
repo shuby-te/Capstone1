c16fc88bc18c8cd84e6139b2bd7c3abb11996197
siruduk <124989831+siruduk@users.noreply.github.com>
2023-10-14 19:49:58 +0900

    231014 update
    보스 움직임 수정
    프리팹 일부 수정

--- BossPrototype/Assets/Scripts/Si_BossMovement.cs
+++ BossPrototype/Assets/Scripts/Si_BossMovement.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
 using static UnityEngine.GraphicsBuffer;
 
 public class Si_BossMovement : MonoBehaviour
@@ -16,101 +18,118 @@ public class Si_BossMovement : MonoBehaviour
     public float lerpSpeed;
 
     Animator anim;
-    bool isNotice = false;
-    bool isAttack = false;
+    public bool isNotice = false;
+    public bool isAttack = false;
+    public bool isActive = false;
+    public bool boomActive = false;
+    public bool move = false;
+    public int attackType;
 
-    float time;
+    public int lastAttackType;
 
     void Start()
     {
         anim = GetComponent<Animator>();
-        BossAttackType1();
-        BossAttackType2();
+        anim.speed += 1.3f;
     }
 
     void Update()
     {
         float dir = Vector3.Distance(transform.position, player.transform.position);
-        if (!isAttack && dir < 8)
+        if (Input.GetKeyDown(KeyCode.Space) && dir < 4)
         {
-
-            isAttack = true;
-            StartCoroutine(TurnHead());
-
+            anim.SetBool("isActive", true);
+            StartCoroutine(deley());
+            isActive = true;
         }
-        if (dir >= 8 && !isAttack)
+        if (!isAttack && isActive)
         {
-            anim.SetInteger("isAttack", 2);
-            Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-            Vector3 t_dir = (playerPos - transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, t_dir, 0.1f);
-            transform.position += transform.forward * speed * Time.deltaTime;
+            isAttack = true;
+            StartCoroutine(Attack());
+        }
+        if ((isActive && !isAttack) || move)
+        {
+            if (move && dir < 4)
+            {
+                move = false;
+                StartCoroutine(AttackType1());
+            }
+            anim.SetBool("isWalk", true);
+            TurnHead();
         }
     }
-    IEnumerator TurnHead()
+    IEnumerator Attack()
     {
-        Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-        anim.SetInteger("isAttack", 2);
-        for (float i = 0; i < 1; i += 0.1f)
+        attackType = UnityEngine.Random.Range(1, 5);
+        if (lastAttackType == attackType)
         {
-            Vector3 t_dir = (playerPos - transform.position).normalized;
-            transform.forward = Vector3.Lerp(transform.forward, t_dir, i);
-            yield return new WaitForFixedUpdate();
+            isAttack = false;
+            yield break;
         }
-        StartCoroutine(SwordAttack());
+        lastAttackType = attackType;
+        switch (attackType)
+        {
+            case 1:
+                move = true;
+                break;
+            case 2:
+                yield return StartCoroutine(AttackType2());
+                StartCoroutine(deley());
+                isAttack = false;
+                break;
+            case 3:
+                if (boomActive) { break; }
+                yield return StartCoroutine(AttackType3());
+                break;
+        }
+        yield break;
+    }
+    IEnumerator deley()
+    {
+        anim.SetBool("isWalk", false);
+        yield return new WaitForSeconds(3);
         yield break;
     }
 
-    IEnumerator SwordAttack()
+    IEnumerator AttackType1()
     {
         anim.SetInteger("isAttack", 1);
-
         yield return new WaitForSeconds(1.3f);
 
         anim.SetInteger("isAttack", 0);
 
         yield return new WaitForSeconds(0.3f);
-
         isAttack = false;
+        StartCoroutine(deley());
         yield break;
     }
 
-    private void OnTriggerEnter(Collider other)
+    IEnumerator AttackType2()
     {
-        //if (other.CompareTag("Player"))
-        //    Destroy(other.gameObject);
-    }
-
-    void BossAttackType1()
-    {
-        StartCoroutine(GearAttack());
-    }
-    IEnumerator GearAttack()
-    {
+        anim.SetInteger("isAttack", 3);
         Vector3 gearPos = transform.localPosition;
         gearPos.x += 3;
         gearPos.y += 3;
         Instantiate(gear, gearPos, gear.transform.rotation);
         yield return new WaitForSeconds(3);
-
+        anim.SetInteger("isAttack", 0);
         for (int i = 1; i < 9; i++)
         {
             gear.transform.Rotate(Vector3.up, 45);
             Quaternion gearRot = gear.transform.rotation;
             Instantiate(miniGear, gearPos, gearRot);
         }
+        yield break;
     }
 
-    void BossAttackType2()
+    IEnumerator AttackType3()
     {
-        StartCoroutine(BombTimer());
-    }
+        anim.SetInteger("isAttack", 3);
 
-    IEnumerator BombTimer()
-    {
         GameObject Bomb = Instantiate(bomb);
+        boomActive = true;
         Bomb.transform.parent = player.transform;
-        Bomb.transform.position = new Vector3(0, 3, 0);
+        Bomb.transform.localPosition = new Vector3(0, 3, 0);
         Renderer renderer = Bomb.GetComponent<Renderer>();
         Color targetColora = Color.blue;
         Color targetColorb = Color.magenta;
@@ -119,6 +138,9 @@ public class Si_BossMovement : MonoBehaviour
         renderer.material.color = targetColora;
 
         yield return new WaitForSeconds(1);
+        isAttack = false;
+        anim.SetInteger("isAttack", 0);
+
         renderer.material.color = targetColorb;
 
         yield return new WaitForSeconds(1);
@@ -126,6 +148,28 @@ public class Si_BossMovement : MonoBehaviour
 
         yield return new WaitForSeconds(1);
         Destroy(Bomb);
+        boomActive = false;
+
         yield break;
+    }
+
+    IEnumerator AttackType4()
+    {
+        anim.SetInteger("isAttack", 4);
+        Vector3 gearPos = transform.position;
+        gearPos.y += 10;
+        Instantiate(gear, gearPos, gear.transform.rotation);
+
+        yield break;
+    }
+
+    private void TurnHead()
+    {
+        Vector3 playerPos = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        for (float i = 0; i < 1; i += 0.1f)
+        {
+            Vector3 t_dir = (playerPos - transform.position).normalized;
+            transform.forward = Vector3.Lerp(transform.forward, t_dir, i);
+        }
     }
 }
