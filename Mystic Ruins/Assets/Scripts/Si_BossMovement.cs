561514466533c4279c874b06f9ee33292a26737f
siruduk <124989831+siruduk@users.noreply.github.com>
2023-10-16 23:56:16 +0900

    임시

--- Mystic Ruins/Assets/Scripts/Si_BossMovement.cs
+++ Mystic Ruins/Assets/Scripts/Si_BossMovement.cs
@@ -29,7 +29,7 @@ public class Si_BossMovement : MonoBehaviour
 
     void Start()
     {
-        Physics.gravity = Physics.gravity * 0;
+        Physics.gravity = Physics.gravity * 10;
         anim = GetComponent<Animator>();
     }
 
