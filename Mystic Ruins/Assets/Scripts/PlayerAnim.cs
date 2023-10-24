047a5fb7e2983626a7b063a4caec7efaf0284be1
shuby_te <101082590+shuby-te@users.noreply.github.com>
2023-10-24 15:20:00 +0900

    231024
    플레이어 이동 애니메이션 및 공격 애니메이션 종합

---
+++ Mystic Ruins/Assets/Scripts/PlayerAnim.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnim : MonoBehaviour
+{    
+    Animator anim;
+    int keyN, mouseN;
+    int isAttack;
+    float time;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (isAttack == 0 && Input.GetMouseButtonDown(0))
+        {
+            time = 0;
+            anim.SetInteger("isAttack", ++isAttack);
+        }
+        else if (time < 0.7f && isAttack == 1 && Input.GetMouseButtonDown(0))
+        {
+            time = 0;
+            anim.SetInteger("isAttack", ++isAttack);
+        }
+        else if (time < 0.7f && isAttack == 2 && Input.GetMouseButtonDown(0))
+        {
+            anim.SetInteger("isAttack", ++isAttack);
+            isAttack = 0;
+        }
+        else if (time > 0.7f)
+        {
+            anim.SetInteger("isAttack", 0);
+            isAttack = 0;
+            time = 0f;
+        }
+
+        float yAngle = transform.rotation.y * 180f;
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+            keyN = 2;
+        else if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+            keyN = 4;
+        else if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
+            keyN = 6;
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+            keyN = 8;
+        else if (Input.GetKey(KeyCode.W))
+            keyN = 1;
+        else if (Input.GetKey(KeyCode.D))
+            keyN = 3;
+        else if (Input.GetKey(KeyCode.S))
+            keyN = 5;
+        else if (Input.GetKey(KeyCode.A))
+            keyN = 7;
+        else
+            keyN = 0;
+
+        if (-22.5f < yAngle && yAngle <= 22.5f)
+            mouseN = 1;
+        else if (22.5f < yAngle && yAngle <= 67.5f)
+            mouseN = 2;
+        else if (67.5f < yAngle && yAngle <= 112.5f)
+            mouseN = 3;
+        else if (112.5f < yAngle && yAngle <= 157.5f)
+            mouseN = 4;
+        else if ((157.5f < yAngle && yAngle <= 180f) || (-180f < yAngle && yAngle <= -157.5f))
+            mouseN = 5;
+        else if (-157.5f < yAngle && yAngle <= -112.5f)
+            mouseN = 6;
+        else if (-112.5f < yAngle && yAngle <= -67.5f)
+            mouseN = 7;
+        else if (-67.5f < yAngle && yAngle <= -22.5f)
+            mouseN = 8;
+
+        Debug.Log(keyN + ", " + mouseN);
+
+        time += Time.deltaTime;
+    }
+
+    private void FixedUpdate()
+    {
+        if (keyN == 0)
+            anim.SetInteger("moveNum", 0);
+        else
+        {
+            int num = mouseN - keyN;
+            if (num > 0) num -= 8;
+            num = -num + 1;
+            anim.SetInteger("moveNum", num);
+        }
+    }   
+}
