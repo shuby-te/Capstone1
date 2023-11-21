e445aa9ef58063a14360c02df0735a11a5991f64
shuby_te <101082590+shuby-te@users.noreply.github.com>
2023-11-21 15:51:13 +0900

    231121 update
    메인씬, 타이틀씬, 개인씬 수정
    아이템 상호작용 및 아이템 저장 기능 추가

--- Mystic Ruins/Assets/Scripts/ItemManager.cs
+++ Mystic Ruins/Assets/Scripts/ItemManager.cs
@@ -1,27 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class ItemManager : MonoBehaviour
 {
-    public int[] items = new int[4];
+    
 
-    void Start()
-    {
-        for (int i = 0; i < items.Length; i++)
+    public void AddItem(GameObject item)
+    {         
+        int[] items = DataManager.Instance.gameData.items;
+        bool isSaved = false;
+        int i = 0;
+        while(i < items.Length) 
         {
-            items[i] = DataManager.Instance.gameData.items[i];
+            Debug.Log("i: " + i);
+            if (items[i] == 0)
+            {
+                DataManager.Instance.gameData.items[i] = item.GetComponent<ItemData>().itemValue;
+                Debug.Log("save: " + i);
+                isSaved = true;
+                break;
+            }
+            i++;
         }
-    }
 
-    void AddItem(GameObject item)   //이 함수를 플레이어 스크립트에서 물체와 상호작용할 때 호출시키도록 함.
-    {
-        int n = 0;
-        while(n < items.Length) 
-        {
-            /*if (items[n] == 0)
-                items[n] = item.GetComponent<ItemScript>().itemNum; //각 획득 가능 아이템마다 식별번호 두고 그 식별번호를 저장
-            */                                                      //혹은 그냥 각 아이템의 프리팹을 저장하는 배열을 두고, 그 배열의 인덱스를 저장
-        }
+        if (!isSaved)
+            Debug.Log("it`s full!");
     }
 }
