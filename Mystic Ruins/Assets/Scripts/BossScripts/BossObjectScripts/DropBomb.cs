b2cc4b5ed36c894cd27cc1ec5e722dc5d8aec5af
siruduk <124989831+siruduk@users.noreply.github.com>
2023-12-12 04:41:39 +0900

    231212 update
    칼 Ai 2종 추가
    게임 멈추는 버그 수정
    원소 패턴 수정
    특수 패턴 오브 회전 추가
    
    뭔가 기타 버그들 수정했는데 기억이안나용

--- Mystic Ruins/Assets/Scripts/BossScripts/BossObjectScripts/DropBomb.cs
+++ Mystic Ruins/Assets/Scripts/BossScripts/BossObjectScripts/DropBomb.cs
@@ -1,17 +1,18 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class DropBomb : MonoBehaviour
+public class DropBomb : BossObject
 {
     public bool isFire = false;
     bool isStart = false;
     public GameObject timer;
+    public ParticleSystem[] particle = new ParticleSystem[1];
 
     // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
-        
+        base.Start();
+        particle[0].gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,14 +26,48 @@ public class DropBomb : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.transform.CompareTag("Boss"))
+        Debug.Log("col");
+        if (collision.transform.CompareTag("Boss"))
         {
+            Debug.Log("bomb");
             if(isFire)
             {
                 StartCoroutine(collision.transform.GetComponent<BossMovement>().Stun(3));
+                collision.transform.GetComponent<BossMovement>().Objmanager.DropBombInactive();
             }
-            Destroy(gameObject);
+            Disable();
         }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        if (other.CompareTag("Boss"))
+        {
+            Debug.Log("bomb");
+            if (isFire)
+            {
+                StartCoroutine(other.transform.parent.GetComponent<BossMovement>().Stun(3));
+                other.transform.parent.GetComponent<BossMovement>().Objmanager.DropBombInactive();
+            }
+            Disable();
+        }
+    }
+    IEnumerator Drop()
+    {
+        particle[0].gameObject.SetActive(true);
+        particle[0].Play();
+        yield return new WaitForSeconds(0.5f);
+        while (gameObject.transform.localPosition.y > -43f)
+        {
+            gameObject.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y - 0.1f, gameObject.transform.localPosition.z);
+            yield return new WaitForFixedUpdate();
+        }
+        particle[0].gameObject.SetActive(false);
+        for (int i = 1; i < 4; i++)
+        {
+            particle[i].Play();
+        }
+        StartCoroutine(Del());
     }
 
     IEnumerator Timer()
@@ -45,6 +80,23 @@ public class DropBomb : MonoBehaviour
             timer.transform.eulerAngles = new Vector3(-90, k++, 0);
             yield return new WaitForSeconds(1 / 120f);
         }
-        Destroy(gameObject);
+            Disable();
+    }
+
+    IEnumerator Del()
+    {
+        yield return new WaitForSeconds(15);
+        Disable();
+    }
+
+    new private void OnEnable()
+    {
+        StartCoroutine(Drop());
+    }
+    new private void OnDisable()
+    {
+        base.OnDisable();
+        StopCoroutine(Timer());
+        isFire = false;
     }
 }
---
+++ Mystic Ruins/Assets/Scripts/BossScripts/BossObjectScripts/SwordTypeA.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SwordTypeA : MonoBehaviour
+{
+    public Transform player; 
+    public float speed = 5f; 
+    public float stopDuration = 2f;
+    bool wait = false;
+    Vector3 movement;
+
+    private void Start()
+    {
+        transform.LookAt(player.position);
+        transform.eulerAngles = new Vector3(90, transform.eulerAngles.y, 0);
+        movement = Vector3.up * Time.deltaTime;
+        StartCoroutine(ChasePlayer());
+    }
+
+    IEnumerator ChasePlayer()
+    {
+        while (true)
+        {
+            transform.Translate(movement);
+            yield return new WaitForFixedUpdate();
+            if (wait)
+                yield return StartCoroutine(Wait());
+        }
+    }
+    IEnumerator Wait()
+    {
+        transform.eulerAngles = new Vector3(0, transform.eulerAngles.y, 0);
+        yield return new WaitForSeconds(stopDuration);
+        transform.LookAt(player.position);
+        transform.eulerAngles = new Vector3(90, transform.eulerAngles.y, 0);
+        movement = Vector3.up * Time.deltaTime * speed;
+        wait = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("wall"))
+            wait = true;
+    }
+}
